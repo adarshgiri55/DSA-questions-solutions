da873e51bb6a19b0f152f4a1fdd21052a5b1da8a
adarshgiri55 <74200600+adarshgiri55@users.noreply.github.com>
2022-11-22 23:49:36 +0530

    Time: 1 ms (86.73%), Space: 41.3 MB (39.91%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
@@ -1,7 +1,7 @@
 class Solution {
     public int trailingZeroes(int n) {
-        long fact =1;
-        int cnt=0,nnt=0;
+        
+        int cnt=0;
 		while(n!=0){
             int temp = n/5;
             cnt+=temp;
